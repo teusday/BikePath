ab88843f891cdf7fc35baf94300c861366c9cf1f
aminPhakhruddin <amin.phakhruddin@gmail.com>
2019-02-23 14:54:57 -0600

    Created Start Activity and Register

--- app/src/main/java/me/zaksharp/bikepath/MainActivity.java
+++ app/src/main/java/me/zaksharp/bikepath/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -12,13 +13,13 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
+
     }
 
     public void toMap(View view){
--- app/src/main/java/me/zaksharp/bikepath/RegisterActivity.java
+++ app/src/main/java/me/zaksharp/bikepath/RegisterActivity.java
@@ -1,13 +1,146 @@
 package me.zaksharp.bikepath;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private TextInputLayout mDisplayName;
+    private  TextInputLayout mEmail;
+    private TextInputLayout mPassword;
+    private Button mCreateBtn;
+
+    private FirebaseAuth mAuth;
+
+    private DatabaseReference mDatabase;
+    private DatabaseReference mUserDatabase;
+
+    private ProgressDialog mRegProgress;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
+
+        mRegProgress = new ProgressDialog(this);
+
+
+
+
+        //Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users");
+
+        mDisplayName = (TextInputLayout) findViewById(R.id.login_email);
+        mEmail = (TextInputLayout) findViewById(R.id.reg_email);
+        mPassword = (TextInputLayout) findViewById(R.id.reg_password);
+        mCreateBtn = (Button) findViewById(R.id.reg_create_btn);
+
+        mCreateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String display_name = mDisplayName.getEditText().getText().toString();
+                String email = mEmail.getEditText().getText().toString();
+                String password = mPassword.getEditText().getText().toString();
+
+                if(!TextUtils.isEmpty(display_name) && !TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)){
+
+                    mRegProgress.setTitle("Registering User");
+                    mRegProgress.setMessage("Please wait while we register we create your account");
+                    mRegProgress.setCanceledOnTouchOutside(false);
+                    mRegProgress.show();
+                    register_user(display_name, email, password);
+
+                }
+
+            }
+        });
+
+
     }
+
+
+
+    private void register_user(final String display_name, String email, String password) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            FirebaseUser current_user = FirebaseAuth.getInstance().getCurrentUser();
+                            String uid = current_user.getUid();
+
+                            mDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(uid);
+
+                            HashMap<String, String> userMap = new HashMap<>();
+                            userMap.put("name", display_name);
+                            userMap.put("status", "Hi there I'm using the Bike Path App");
+                            userMap.put("image","default");
+                            userMap.put("thumb_image", "default");
+
+                            mDatabase.setValue(userMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        mRegProgress.dismiss();
+
+                                        String currentUserId = mAuth.getCurrentUser().getUid();
+                                        String deviceToken = FirebaseInstanceId.getInstance().getToken();
+
+                                        mUserDatabase.child(currentUserId).child("device_token").setValue(deviceToken).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                            @Override
+                                            public void onSuccess(Void aVoid) {
+                                                Intent mainIntent = new Intent( RegisterActivity.this, MainActivity.class);
+                                                mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                                                startActivity(mainIntent);
+                                                finish();
+                                            }
+                                        });
+
+                                    }
+                                }
+                            });
+
+
+
+
+                        } else {
+                            //can get more details regarding the error
+                            mRegProgress.hide();
+                            Toast.makeText(RegisterActivity.this, "Cannot Sign in. Please the form and try again.", Toast.LENGTH_LONG).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+
 }
