a0665f2e94d98c16fb036f83b17d209c092932f3
Zak Sharp <macmaster195@gmail.com>
2019-02-23 12:52:49 -0600

    Provided a way to get to the map activity

--- app/src/main/java/me/zaksharp/bikepath/MapsActivity.java
+++ app/src/main/java/me/zaksharp/bikepath/MapsActivity.java
@@ -43,7 +43,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        if(checkSelfPermission("android.permission.ACCESS_FINE_LOCATION")== PackageManager.PERMISSION_GRANTED)
-            mMap.setMyLocationEnabled(true);
+        switch(checkSelfPermission("android.permission.ACCESS_FINE_LOCATION")) {
+            case PackageManager.PERMISSION_GRANTED:
+                mMap.setMyLocationEnabled(true);
+                break;
+            case PackageManager.PERMISSION_DENIED:
+                requestPermissions(new String[]{"android.permission.ACCESS_FINE_LOCATION"},0);
+                break;
+            default:
+                break;
+        }
     }
 }
