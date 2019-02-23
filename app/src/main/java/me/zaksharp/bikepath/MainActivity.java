758e66e36dbe1fcefd0c1a536a685a9a6ce900ec
aminPhakhruddin <amin.phakhruddin@gmail.com>
2019-02-23 14:17:19 -0600

    Created Start Activity

--- app/src/main/java/me/zaksharp/bikepath/MainActivity.java
+++ app/src/main/java/me/zaksharp/bikepath/MainActivity.java
@@ -1,13 +1,44 @@
 package me.zaksharp.bikepath;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
     }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser == null){
+
+            sendToStart();
+
+        }
+
+    }
+
+    private void sendToStart(){
+        Intent startIntent = new Intent(this, StartActivity.class);
+        startActivity(startIntent);
+        finish();
+    }
+
 }
